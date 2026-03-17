923e5fd31014de20185ea0d08dd3a85fd3b0ab07
Durga Prasanna <prasannadurga689@gmail.com>
2026-03-17 17:09:12 +0530

    Add project files.

---
+++ WindowsProjet/Form1.cs
@@ -0,0 +1,23 @@
+namespace WindowsProjet
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Button b= new Button();
+            b.Text = "Click me";
+            b.Size = new Size(100, 50);
+            b.Location = new Point(100, 100); 
+            this.Controls.Add(b);
+            b.Click += delegate { 
+                MessageBox.Show("Button clicked!");
+            };
+        }
+       
+    }
+}
